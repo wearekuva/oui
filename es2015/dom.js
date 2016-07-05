38492c56185bae2a00980e53df99bb5e8d14a6ad
Mark Lundin <mark@mark-lundin.com>
2016-07-05 22:00:14 +0100

    removed dead code

--- es2015/dom.js
+++ es2015/dom.js
@@ -5,13 +5,7 @@ document.body.appendChild( element )
 element.style.position = 'absolute'
 element.style.left = '0'
 element.style.top = '0'
-
-// element.style.width = '100%'
-// element.style.height = '100%'
-
-element.style.padding = '0.25em'
-// element.style.margin = '0.5em'
-
+element.style.padding = '0.25em''
 element.style.display = 'flex'
 element.style.flexDirection = 'column'
 element.style.flexWrap = 'wrap'
