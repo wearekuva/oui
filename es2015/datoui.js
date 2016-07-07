8eed25cd69f3036c1f4758a73af711592f1f67fd
Mark Lundin <mark@mark-lundin.com>
2016-07-07 17:47:56 +0100

    formatting

--- es2015/datoui.js
+++ es2015/datoui.js
@@ -1,23 +1,23 @@
-import oui from 'oui'
+import panel from './imperative-api'
 
 let add = ( obj, propName, target ) => Object.defineProperty( target, propName, {
-  get: _ => obj[propName],
-  set: v => obj[propName] = v,
-  enumerable: true, configurable: true,
+    get: _ => obj[propName],
+    set: v => obj[propName] = v,
+    enumerable: true, configurable: true,
 });
 
 let addFolder = target => ({
-  add: ( obj, propName ) => add( obj, propName, target ),
-  addFolder: propName => addFolder( target[propName] = {} )
+    add: ( obj, propName ) => add( obj, propName, target ),
+    addFolder: propName => addFolder( target[propName] = {} )
 })
 
 export default opts => {
-  let api = {}
-  let p = oui.panel(opts)
-  let draw = _ => {
-    p( api )
-    requestAnimationFrame( draw )
-  }
-  draw()
-  return addFolder( api )
+    let api = {}
+    let p = panel(opts)
+    let draw = _ => {
+        p( api )
+        requestAnimationFrame( draw )
+    }
+    draw()
+    return addFolder( api )
 }
