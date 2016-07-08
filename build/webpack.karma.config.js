be35ca46a43cb1424d43a86edceeac84c9363a4f
Mark Lundin <mark@mark-lundin.com>
2016-07-08 15:07:21 +0100

    Tests are no longer transpiled. Just polyfilling es6 imports

--- build/webpack.karma.config.js
+++ build/webpack.karma.config.js
@@ -4,23 +4,23 @@ var webpack = require('webpack')
 
 module.exports = {
     // devtool: 'inline-source-map',
-    // externals: {
-    //   'react/addons': true,
-    //   'react/lib/ExecutionEnvironment': true,
-    //   'react/lib/ReactContext': true
-    // },
     module: {
         loaders:[
             { test: /\.json$/, loader: "json-loader" },
             {
                 loader: 'babel-loader',
+                babelrc: false,
                 exclude: /(node_modules|bower_components)/,
                 query: {
-                //     // compact: false,
-                    // babelrc: false,
                     presets: [/*"es2015",*/  'react' ],
-                    "plugins": ["transform-es2015-modules-commonjs", "transform-object-rest-spread"]
-                //     // plugins: ["]
+                    "plugins": [
+                        "transform-es2015-modules-commonjs",
+                        "transform-object-rest-spread",
+                        ["transform-runtime", {
+                            "polyfill": false,
+                            "regenerator": false
+                        }]
+                    ]
                 }
             }
         ],
@@ -30,7 +30,7 @@ module.exports = {
           exclude: /(\S*\.test\.js|node_modules)/,
           loader: 'istanbul-instrumenter'
         }]
-    }),
+    },
 
     plugins: [
       new webpack.DefinePlugin({
