dbc8aa2f1985faac4e28f567a34be8a7dc368b10
Mark Lundin <mark@mark-lundin.com>
2016-09-29 11:25:52 +0100

    expose numeric stepper

--- es2015/oui.js
+++ es2015/oui.js
@@ -5,10 +5,11 @@ import datoui from './datoui'
 import ColorPicker, { color } from './controls/colorpicker'
 import ComboBox, { combobox } from './controls/combobox'
 import Graph, { graph } from './controls/graph'
+import NumericStepper, { numericstepper } from './controls/numericstepper'
 import XYPad, { xypad } from './controls/xypad'
 
 let oui = panel({label:'Master'})
 
-const controls = { ColorPicker, ComboBox, Graph, XYPad }
+const controls = { ColorPicker, ComboBox, Graph, NumericStepper, XYPad }
 
-export default { oui, panel, datoui, annotate, color, combobox, graph, xypad, controls }
+export default { oui, panel, datoui, annotate, color, combobox, graph, numericstepper, xypad, controls }
